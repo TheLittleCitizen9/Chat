63919ebbeba3dc31cd68c927f6d1dd114441d2af
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-07 17:57:17 +0300

    Created abstract class to represent basic chat

---
+++ ChatClient/ChatClient/Chats/BasicChat.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.Chats
+{
+    public abstract class BasicChat
+    {
+        protected Byte[] _bytesReceived;
+        protected TcpClient _client;
+        protected ConsoleDisplayer _consoleDisplayer;
+
+        public BasicChat(Byte[] bytes, TcpClient tcpClient, ConsoleDisplayer consoleDisplayer)
+        {
+            _bytesReceived = bytes;
+            _client = tcpClient;
+            _consoleDisplayer = consoleDisplayer;
+        }
+
+        public virtual void Run()
+        {
+            ReadFromServer();
+        }
+
+        public virtual void ShowOptions()
+        {
+
+        }
+        public virtual void WriteMessage(string message)
+        {
+            if (message == "exit")
+            {
+                Exit();
+            }
+            else
+            {
+                NetworkStream nwStream = _client.GetStream();
+                byte[] messageToSend = Encoding.ASCII.GetBytes(message);
+                nwStream.Write(messageToSend);
+            }
+        }
+        public virtual void ReadFromServer()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        NetworkStream nwStream = _client.GetStream();
+                        nwStream.Read(_bytesReceived, 0, _bytesReceived.Length);
+                        _consoleDisplayer.PrintValueToConsole(Encoding.ASCII.GetString(_bytesReceived));
+                    }
+                    catch (IOException e)
+                    {
+                        _consoleDisplayer.PrintValueToConsole($"Server disconnected");
+                        Exit();
+                    }
+                }
+            });
+        }
+
+        private void Exit()
+        {
+            _client.Close();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ ChatClient/ChatClient/Chats/GlobalChat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace ChatClient.Chats
+{
+    public class GlobalChat : BasicChat
+    {
+        public GlobalChat(Byte[] bytes, TcpClient tcpClient, ConsoleDisplayer consoleDisplayer) 
+            :base(bytes, tcpClient, consoleDisplayer)
+        {
+
+        }
+
+        public override void ShowOptions()
+        {
+            Dictionary<string, string> options = new Dictionary<string, string>();
+            options.Add("return", "return to main menu");
+            options.Add("exit", "exit program");
+            _consoleDisplayer.PrintMenu(options);
+        }
+
+        public void WriteMessage()
+        {
+            while (true)
+            {
+                _consoleDisplayer.PrintValueToConsole("Enter message to send");
+                string message = Console.ReadLine();
+                if(message == "return")
+                {
+                    base.WriteMessage(message);
+                    break;
+                }
+                base.WriteMessage(message);
+            }
+        }
+        public override void Run()
+        {
+            base.ReadFromServer();
+            WriteMessage();
+        }
+    }
+}
