b9df0c36488557af0976bd233d0f764236055bfd
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-07 17:58:57 +0300

    Created a class to represent user

---
+++ ChatServer/ChatServer/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer
+{
+    public class User
+    {
+        public TcpClient ClientSocket;
+        public List<TcpClient> ClientsInChatWith;
+        public int Id;
+
+        public User(TcpClient tcpClient, int id)
+        {
+            ClientSocket = tcpClient;
+            ClientsInChatWith = new List<TcpClient>();
+            Id = id;
+        }
+
+        public void RemoveClient(TcpClient tcpClient)
+        {
+            if(ClientsInChatWith.Contains(tcpClient))
+            {
+                ClientsInChatWith.Remove(tcpClient);
+            }
+        }
+    }
+}
