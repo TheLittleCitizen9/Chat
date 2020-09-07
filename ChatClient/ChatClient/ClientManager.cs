78541feede41044669a3772972bfb08a532915c8
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-07 17:59:47 +0300

    Created class to manage the client's actions

---
+++ ChatClient/ChatClient/ClientManager.cs
@@ -0,0 +1,40 @@
+﻿using ChatClient.Chats;
+using System;
+using System.Collections.Generic;
+
+namespace ChatClient
+{
+    public class ClientManager
+    {
+        private Dictionary<string, string> _dispalyOptions;
+        private ConsoleDisplayer _consoleDisplayer;
+        private Client _client;
+
+        public ClientManager(Dictionary<string, string> options)
+        {
+            _dispalyOptions = options;
+            _consoleDisplayer = new ConsoleDisplayer();
+            _client = new Client();
+        }
+
+        public void InitializeClient()
+        {
+            _client.CreateClient();
+        }
+
+        public void NavigateToChoice()
+        {
+            while (true)
+            {
+                _consoleDisplayer.PrintMenu(_dispalyOptions);
+                string option = Console.ReadLine();
+                if (option == "1")
+                {
+                    _client.CurrentChat = new GlobalChat(_client.BytesReceived, _client.TcpClient, _client.ConsoleDisplayer);
+                    _client.CurrentChat.ShowOptions();
+                    _client.CurrentChat.Run();
+                }
+            }
+        }
+    }
+}
