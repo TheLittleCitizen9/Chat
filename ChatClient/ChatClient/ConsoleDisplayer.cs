5e403b1780df4f970c19a2e365ada6162b172001
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-07 14:37:10 +0300

    Created ConsoleDisplayer class

---
+++ ChatClient/ChatClient/ConsoleDisplayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClient
+{
+    public class ConsoleDisplayer
+    {
+        public void PrintValueToConsole(object obj)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
