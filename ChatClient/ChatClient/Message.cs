2c7721c4f4ddf1ceddad39e659a75717e5793dce
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-08 13:42:50 +0300

    A class to represent the message that the client/server send

---
+++ ChatClient/ChatClient/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClient
+{
+    [Serializable]
+    public class Message<T>
+    {
+        public Guid ChatId { get; set; }
+        public T MessageToSend { get; set; }
+    }
+}
