af6c9afade396c98eb14cd19dc1369c1343b99a4
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-08 13:37:13 +0300

    Added chat managers to habdle the each chat in a different class

---
+++ ChatServer/ChatServer/ChatManagers/BasicChatManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer.ChatManagers
+{
+    public class BasicChatManager
+    {
+        protected List<User> _clients;
+        protected ConsoleDisplayer _consoleDisplayer;
+        protected Dictionary<Guid, List<User>> _usersInChats;
+        public BasicChatManager(Dictionary<Guid, List<User>> usersInChats, List<User> clients)
+        {
+            _usersInChats = usersInChats;
+            _clients = clients;
+            _consoleDisplayer = new ConsoleDisplayer();
+        }
+        public string GetDataFromClient(User user)
+        {
+            byte[] buffer = new byte[1024];
+            NetworkStream nwStream = user.ClientSocket.GetStream();
+            int bytesRecieved = nwStream.Read(buffer);
+            string stringData = Encoding.ASCII.GetString(buffer);
+            return stringData;
+        }
+
+        public string DisconnectClient(User user)
+        {
+            string clientDisconnectedMsg = $"Client {user.ClientSocket.Client.RemoteEndPoint} disconnected";
+            _consoleDisplayer.PrintValueToConsole(clientDisconnectedMsg);
+            RemoveClient(user);
+            return clientDisconnectedMsg;
+        }
+
+        public void RemoveClient(User user, Guid chatId = default)
+        {
+            if (chatId == default)
+            {
+                RemoveUserFromAllChats(user);
+            }
+            else
+            {
+                _usersInChats[chatId].Remove(user);
+            }
+        }
+
+        private void RemoveUserFromAllChats(User user)
+        {
+            _clients.Remove(user);
+            foreach (KeyValuePair<Guid, List<User>> pair in _usersInChats)
+            {
+                foreach (var usr in pair.Value)
+                {
+                    if (usr == user)
+                    {
+                        pair.Value.Remove(usr);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ChatServer/ChatServer/ChatManagers/GlobalChatManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer.ChatManagers
+{
+    public class GlobalChatManager : BasicChatManager
+    {
+        private Guid _globalChatId;
+        public List<User> UsersInChat;
+
+        public GlobalChatManager(Guid id, Dictionary<Guid, List<User>> usersInChats, List<User> clients)
+            :base(usersInChats, clients)
+        {
+            _globalChatId = id;
+            UsersInChat = new List<User>();
+        }
+        public void ChatWithClientInGlobalChat(User user)
+        {
+            try
+            {
+                while (true)
+                {
+                    string dataFromClient = GetDataFromClient(user);
+                    string noNullValuesData = dataFromClient.Replace("\0", string.Empty);
+                    if (noNullValuesData == "return")
+                    {
+                        user.AddNumbChatId(_globalChatId);
+                        RemoveClient(user, _globalChatId);
+                        SendMessageToAllClients($"Client {user.Id} left chat");
+                        break;
+                    }
+                    string messageToClients = $"Client {user.Id} - {dataFromClient}";
+                    SendMessageToAllClients(messageToClients);
+                }
+            }
+            catch (Exception)
+            {
+                DisconnectClient(user);
+            }
+        }
+        public void SendMessageToAllClients(string dataToSend)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(dataToSend);
+            foreach (var client in UsersInChat)
+            {
+                try
+                {
+                    NetworkStream nwStream = client.ClientSocket.GetStream();
+                    nwStream.Write(data);
+                }
+                catch (Exception e)
+                {
+                    _consoleDisplayer.PrintValueToConsole(e.Message);
+                }
+            }
+        }
+
+        public void EnterUserToGlobalChat(User user)
+        {
+            user.AddActiveChatId(_globalChatId);
+            UsersInChat.Add(user);
+            string clientConnectedMsg = $"Client {user.Id} connected";
+            SendMessageToAllClients(clientConnectedMsg);
+            ChatWithClientInGlobalChat(user);
+        }
+    }
+}
