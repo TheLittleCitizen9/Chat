f7a1574a5aa92ee114a29f0427da20ab8288670e
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-08 13:58:39 +0300

    Created a new solution touse as dll for combined objects

---
+++ BasicChatContents/BasicChatContents/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BasicChatContents
+{
+    [Serializable]
+    public class Message<T>
+    {
+        public Guid ChatId { get; set; }
+        public T MessageToSend { get; set; }
+    }
+}
---
+++ BasicChatContents/BasicChatContents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicChatContents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
