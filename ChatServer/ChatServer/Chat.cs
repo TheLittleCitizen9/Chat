7e282a41064012dbe7524ff9788fce9a85011227
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-08 15:51:37 +0300

    Created class to represent a chat

---
+++ ChatServer/ChatServer/Chat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatServer
+{
+    public class Chat
+    {
+        public string Name { get; set; }
+        public Guid Id { get; set; }
+
+        public ChatOptions ChatOption { get; set; }
+
+        public Chat(string name, Guid id, ChatOptions option)
+        {
+            Name = name;
+            Id = id;
+            ChatOption = option;
+        }
+    }
+}
