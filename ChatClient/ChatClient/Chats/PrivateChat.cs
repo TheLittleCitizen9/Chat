880b2cab49ae6f4da04f46b4244fedd28357966f
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-08 15:51:11 +0300

    Created class to represent a private chat

---
+++ ChatClient/ChatClient/Chats/PrivateChat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient.Chats
+{
+    public class PrivateChat : BasicChat
+    {
+        public PrivateChat(Byte[] bytes, TcpClient tcpClient, ConsoleDisplayer consoleDisplayer)
+            : base(bytes, tcpClient, consoleDisplayer)
+        {
+
+        }
+
+        public override void ShowOptions()
+        {
+            Dictionary<string, string> options = new Dictionary<string, string>();
+            options.Add("return", "return to main menu");
+            options.Add("exit", "exit program");
+            _consoleDisplayer.PrintMenu(options);
+        }
+
+        public void WriteMessage()
+        {
+            while (true)
+            {
+                _consoleDisplayer.PrintValueToConsole("Enter message to send");
+                string message = Console.ReadLine();
+                if (message == "return")
+                {
+                    base.WriteMessage(message);
+                    break;
+                }
+                base.WriteMessage(message);
+            }
+        }
+
+        public void ChooseClientToTalkTo()
+        {
+
+        }
+    }
+}
