806fe684eb3508f2fa519e52d5ff3d1da2930076
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-08 13:41:50 +0300

    Enum that represents the chat options

---
+++ ChatClient/ChatClient/ChatOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClient
+{
+    public enum ChatOptions
+    {
+        Global = 1,
+        Private = 2,
+        Group = 3
+    }
+}
