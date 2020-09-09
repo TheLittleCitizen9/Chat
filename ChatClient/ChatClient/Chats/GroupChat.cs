898648bf555bbbb975deebe72d26f23868e525f1
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-09 11:42:45 +0300

    Class to handle the group chat functions

---
+++ ChatClient/ChatClient/Chats/GroupChat.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClient.Chats
+{
+    public class GroupChat : BasicChat
+    {
+        private const string NO_OTHER_USERS_CONNECTED = "No other users connected";
+        public GroupChat(Byte[] bytes, Client tcpClient, ConsoleDisplayer consoleDisplayer)
+            : base(bytes, tcpClient, consoleDisplayer)
+        {
+
+        }
+
+        public override void Run()
+        {
+            string allClientsConnected = _client.GetMessageFromServer();
+            if (ChooseClientToTalkTo(allClientsConnected))
+            {
+                EnterChatName();
+                base.ReadFromServer();
+                ShowOptions();
+                WriteMessage();
+            }
+        }
+
+        public bool ChooseClientToTalkTo(string allClientsConnected)
+        {
+            string[] allClients = allClientsConnected.Split(',');
+            if (allClients[0].Replace("\0", string.Empty) != NO_OTHER_USERS_CONNECTED)
+            {
+                PrintClientsConnected(allClients);
+                _consoleDisplayer.PrintValueToConsole("Enter client IDs to talk to (put ',' between IDs)");
+                string ids = Console.ReadLine();
+                string[] clientsToAdd = ids.Split(',');
+                if (ValidateIds(clientsToAdd, allClients))
+                {
+                    SendServerClientIds(ids);
+                    return true;
+                }
+                else
+                {
+                    _consoleDisplayer.PrintValueToConsole("Please enter valid ids");
+                    return false;
+                }
+            }
+            else
+            {
+                _consoleDisplayer.PrintValueToConsole("Please choose a different option - there are no other users connected");
+                return false;
+            }
+        }
+
+        private void EnterChatName()
+        {
+            _consoleDisplayer.PrintValueToConsole("Enter Chat Name");
+            string chatName = Console.ReadLine();
+            base.WriteMessage(chatName);
+        }
+
+        private void PrintClientsConnected(string[] clients)
+        {
+            foreach (var client in clients)
+            {
+                _consoleDisplayer.PrintValueToConsole(client);
+            }
+        }
+
+        private void SendServerClientIds(string ids)
+        {
+            base.WriteMessage(ids);
+        }
+
+        private bool ValidateIds(string[] ids, string[] allClients)
+        {
+            bool result = false;
+            foreach (var client in allClients)
+            {
+                foreach (var id in ids)
+                {
+                    if (client.Contains(id))
+                        result = true;
+                }
+            }
+            return result;
+        }
+    }
+}
